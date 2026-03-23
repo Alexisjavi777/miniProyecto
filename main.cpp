75a6f5c9cd625e2d7e7dce74df20e706d36e71ea
Alexisjavi777 <javiercayo25@gmail.com>
2026-03-22 20:55:47 -0500

    Crear funciones de sistema de inventario

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+MINI PROYECTO C++
+Autor: Alexis Liquinchana
+Fecha: 2026/03/23
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void registrarProducto(string nombres[], int cantidades[], float precios[], int &contador);
+void mostrarInventario(string nombres[], int cantidades[], float precios[], int contador);
+void subMenuReportes(string nombres[], int cantidades[], float precios[], int contador);
+void buscarProducto(string nombres[], int cantidades[], float precios[], int contador);
+
+
+
+
+
